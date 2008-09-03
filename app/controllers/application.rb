2b585e8d0aa26a47a1738b694c09988290d8771d
Ben Scofield <git@turrean.com>
2008-09-03 08:34:24 +0200

    remove protect_from_forgery for AJAX

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => '3ad7dc40060996283a3490161ebdbad1'
+  # protect_from_forgery # :secret => '3ad7dc40060996283a3490161ebdbad1'
   
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
